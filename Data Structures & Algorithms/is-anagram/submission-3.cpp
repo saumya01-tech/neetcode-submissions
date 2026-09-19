a8afb5e99e232adbd4e5331ed61f1ff503244290
Saumya Jangid <saumya10.jangid@gmail.com>
2026-09-19 17:31:56 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // both strings should have same length
+        if(s.size()!= t.size()){
+            return false;
+        }
+        int n=s.size();
+        unordered_map<char,int> map_s, map_t;
+
+        for(int i=0;i<n;i++){
+            map_s[s[i]]++;
+            map_t[t[i]]++;
+        }
+        
+        return map_s==map_t;
+    }
+};
